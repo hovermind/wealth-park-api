3d21d9dadf6e57f64625590c51a275cbe7806f0c
HASSAN MD TAREQ <console041@gmail.com>
2019-08-25 17:12:16 +0900

    Add entity and dbcontext

---
+++ WealthPark/WealthParkApi/Domain/EmployeeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WealthParkApi.Domain.Entities;
+
+namespace WealthParkApi.Domain
+{
+    /// <summary>
+    /// Employee DbContext
+    /// </summary>
+    public class EmployeeDbContext : DbContext
+    {
+        /// <summary>
+        /// DbContext ctor for DI
+        /// </summary>
+        /// <param name="options"></param>
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// Employees DB set
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WealthPark/WealthParkApi/Domain/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WealthParkApi.Domain.Entities
+{
+    /// <summary>
+    /// Employee entity
+    /// </summary>
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ConcurrencyCheck]
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(15), MinLength(3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(15), MinLength(3)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Boss { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
