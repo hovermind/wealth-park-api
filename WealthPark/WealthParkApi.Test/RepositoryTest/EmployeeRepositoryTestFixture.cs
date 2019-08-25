667f61bfc090cc9c99442e7263bdefb0db4a7559
HASSAN MD TAREQ <console041@gmail.com>
2019-08-25 17:29:48 +0900

    Add test fixure

---
+++ WealthPark/WealthParkApi.Test/RepositoryTest/EmployeeRepositoryTestFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using WealthParkApi.Domain;
+
+namespace WealthParkApi.Test.RepositoryTest
+{
+    public class EmployeeRepositoryTestFixture : IDisposable
+    {
+        public DbContextOptions<EmployeeDbContext> DbContextOptions { get; private set; }
+        private SqliteConnection _connection;
+
+        public EmployeeRepositoryTestFixture()
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+            var connectionString = connectionStringBuilder.ToString();
+            _connection = new SqliteConnection(connectionString);
+            //_connection.Open();
+
+            // create in-memory context
+            var builder = new DbContextOptionsBuilder<EmployeeDbContext>();
+            builder.UseSqlite(_connection); //builder.UseInMemoryDatabase(databaseName: "wealthpark");
+
+            DbContextOptions = builder.Options;
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
---
+++ WealthPark/WealthParkApi.Test/SQLiteInMemoryOptionsBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WealthParkApi.Test
+{
+    /// <summary>
+    /// DbContext option bulder for SQLite in-memory DB
+    /// </summary>
+    public class SQLiteInMemoryOptionsBuilder
+    {
+        /// <summary>
+        /// Creates DbContext option for SQLite in-memory DB
+        /// </summary>
+        /// <typeparam name="T">Type of DbContext</typeparam>
+        /// <param name="throwOnClientServerWarning"></param>
+        /// <returns></returns>
+        public static DbContextOptions<T> CreateOptions<T>(bool throwOnClientServerWarning = true) where T : DbContext
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            // create in-memory context
+            var builder = new DbContextOptionsBuilder<T>();
+            builder.UseSqlite(connection);
+            //builder.ApplyOtherOptionSettings(throwOnClientServerWarning);
+            return builder.Options;
+        }
+    }
+}
